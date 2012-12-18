625d96391a417d742132eb4031515176b9a33691
Magnar Sveen <magnars@gmail.com>
2012-12-18 16:30:47 +0100

    Add commentary and version

--- fold-this.el
+++ fold-this.el
@@ -3,6 +3,7 @@
 ;; Copyright (C) 2012 Magnar Sveen <magnars@gmail.com>
 
 ;; Author: Magnar Sveen <magnars@gmail.com>
+;; Version: 0.1.0
 ;; Keywords: convenience
 
 ;; This program is free software; you can redistribute it and/or
@@ -19,6 +20,27 @@
 ;; along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
+
+;; Just fold the active region, please.
+;;
+;; ## How it works
+;;
+;; The command `fold-this` visually replaces the current region with `...`.
+;; If you move point into the ellipsis and press enter or `C-g` it is unfolded.
+;;
+;; You can unfold everything with `fold-this-unfold-all`.
+;;
+;; You can fold all instances of the text in the region with `fold-this-all`.
+;;
+;; ## Setup
+;;
+;; I won't presume to know which keys you want these functions bound to,
+;; so you'll have to set that up for yourself. Here's some example code,
+;; which incidentally is what I use:
+;;
+;;     (global-set-key (kbd "C-c C-f") 'fold-this-all)
+;;     (global-set-key (kbd "C-c C-F") 'fold-this)
+;;     (global-set-key (kbd "C-c M-f") 'fold-this-unfold-all)
 
 ;;; Code:
 
